9c50fa4031fabeaf01e7266b2e81ca3168ba72c2
alonsoagueda <143227252+alonsoagueda@users.noreply.github.com>
2023-10-05 00:20:56 +0000

    second

--- src/app.js
+++ src/app.js
@@ -6,6 +6,27 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let generateExcuse = () => {
+    let who = ["The cow", "A sheep", "Some old lady", "My dog"];
+    let verb = ["ate", "destroyed", "burned", "threw away"];
+    let what = ["my phone", "my keys", "my homework", "my laptop"];
+    let when = ["this morning.", "before coming to work", "last night"];
+
+    let whoIndex = 0;
+    let verbIndex = 0;
+    let whatIndex = 0;
+    let whenIndex = 0;
+
+    return (
+      who[whoIndex] +
+      "" +
+      verb[verbIndex] +
+      "" +
+      what[whatIndex] +
+      "" +
+      when[whenIndex]
+    );
+  };
 };
+
+console.log("Hello Rodrigo");
